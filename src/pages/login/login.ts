508cf45cf25ba52cf5e8353bd456db98323bf3d3
Rafael do Prado Ferreira <faeotc@gmail.com>
2018-08-29 21:42:07 -0300

    conexao com web service -- teste

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+
+
 /**
  * Generated class for the LoginPage page.
  *
@@ -24,6 +26,7 @@ export class LoginPage {
     this.http.get(this.url).map(res => res.json())
       .subscribe(data => {
         this.feeds = data.pessoas;
+        //console.log(data);
       }); 
   
   }
